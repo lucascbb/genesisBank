065642506357b9f97064c4b3f23278d2e7e7f41f
lucascbb <lucasbaroquello@hotmail.com>
2023-06-19 10:05:14 -0300

    :style: css + tests

---
+++ src/tests/mockBook.js
@@ -0,0 +1,74 @@
+const mockBook = {
+  kind: 'books#volume',
+  id: 'MWkOEAAAQBAJ',
+  etag: 'utOIHE4AtQg',
+  selfLink: 'https://www.googleapis.com/books/v1/volumes/MWkOEAAAQBAJ',
+  volumeInfo: {
+    title: 'React Aprenda Praticando',
+    subtitle: 'Desenvolva aplicações web reais com uso da biblioteca React e de seus módulos auxiliares',
+    authors: [
+      'Mauricio Samy Silva'
+    ],
+    publisher: 'Novatec Editora',
+    publishedDate: '2021-01-05',
+    description: 'React é uma biblioteca para a criação de sites, interfaces gráficas e aplicações web, criada pelo Facebook, e seu uso tem crescido muito, sendo usada por grandes empresas, como Netflix, Walmart e The New York Times. Neste livro, eminentemente prático, Maujor, com sua reconhecida didática, fornece ao leitor uma visão detalhada dos conceitos básicos e recursos da biblioteca React. Você aprenderá a desenvolver aplicativos React completos, passo a passo, desde o zero até a hospedagem em um servidor remoto. Cada capítulo apresenta um novo recurso do React, com exercícios práticos para consolidar os conceitos estudados. Destina-se a desenvolvedores com conhecimentos básicos de HTML, CSS3 e JavaScript, interessados na criação de sites tanto na área de design quanto na de desenvolvimento e programação. O livro também poderá ser útil como material de referência do React. A estrutura de pastas dos projetos desenvolvidos no livro encontra-se em https://github.com/Maujor/livro-react e o respectivo material de apoio aos projetos está disponível no site do livro em https://kwz.me/h1m.',
+    industryIdentifiers: [
+      {
+        type: 'ISBN_13',
+        identifier: '9786586057393'
+      },
+      {
+        type: 'ISBN_10',
+        identifier: '6586057396'
+      }
+    ],
+    readingModes: {
+      text: false,
+      image: false
+    },
+    pageCount: 33,
+    printType: 'BOOK',
+    categories: [
+      'Business & Economics'
+    ],
+    maturityRating: 'NOT_MATURE',
+    allowAnonLogging: false,
+    contentVersion: '0.0.1.0.preview.0',
+    panelizationSummary: {
+      containsEpubBubbles: false,
+      containsImageBubbles: false
+    },
+    imageLinks: {
+      smallThumbnail: 'http://books.google.com/books/content?id=MWkOEAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+      thumbnail: 'http://books.google.com/books/content?id=MWkOEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+    },
+    language: 'pt-BR',
+    previewLink: 'http://books.google.com.br/books?id=MWkOEAAAQBAJ&printsec=frontcover&dq=react&hl=&cd=1&source=gbs_api',
+    infoLink: 'http://books.google.com.br/books?id=MWkOEAAAQBAJ&dq=react&hl=&source=gbs_api',
+    canonicalVolumeLink: 'https://books.google.com/books/about/React_Aprenda_Praticando.html?hl=&id=MWkOEAAAQBAJ'
+  },
+  saleInfo: {
+    country: 'BR',
+    saleability: 'NOT_FOR_SALE',
+    isEbook: false
+  },
+  accessInfo: {
+    country: 'BR',
+    viewability: 'PARTIAL',
+    embeddable: true,
+    publicDomain: false,
+    textToSpeechPermission: 'ALLOWED',
+    epub: {
+      isAvailable: false
+    },
+    pdf: {
+      isAvailable: false
+    },
+    webReaderLink: 'http://play.google.com/books/reader?id=MWkOEAAAQBAJ&hl=&source=gbs_api',
+    accessViewStatus: 'SAMPLE',
+    quoteSharingAllowed: false
+  },
+  searchInfo: {
+    textSnippet: 'React é uma biblioteca para a criação de sites, interfaces gráficas e aplicações web, criada pelo Facebook, e seu uso tem crescido muito, sendo usada por grandes empresas, como Netflix, Walmart e The New York Times.'
+  }
+};
